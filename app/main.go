4b488630d9307807d6946006ef435b64ca81e4cb
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2022-11-19 18:46:33 +0900

    #001 Add web server start

--- app/main.go
+++ app/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var e = createMux()
 
 func main() {
-	fmt.Println("Hello World!")
+	e.GET("/", articleIndex)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	return e
+}
+
+func articleIndex(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
 }
