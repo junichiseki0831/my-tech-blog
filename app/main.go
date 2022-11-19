8bb507168fb6c4a74fdae01aeabddfc869208ad8
junichiseki0831 <soulfly_0831@yahoo.co.jp>
2022-11-19 20:43:42 +0900

    #005 Add routing

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+  "strconv"
 	"time"
 
 	"github.com/flosch/pongo2"
@@ -15,6 +16,9 @@ var e = createMux()
 
 func main() {
 	e.GET("/", articleIndex)
+  e.GET("/new", articleNew)
+  e.GET("/:id", articleShow)
+  e.GET("/:id/edit", articleEdit)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
@@ -34,10 +38,44 @@ func createMux() *echo.Echo {
 
 func articleIndex(c echo.Context) error {
   data := map[string]interface{}{
-    "Message": "Hello, World!",
+    "Message": "Article Index",
     "Now":     time.Now(),
   }
   return render(c, "article/index.html", data)
+}
+
+func articleNew(c echo.Context) error {
+  data := map[string]interface{}{
+    "Message": "Article New",
+    "Now":     time.Now(),
+  }
+
+  return render(c, "article/new.html", data)
+}
+
+func articleShow(c echo.Context) error {
+  // パスパラメータ抽出
+  id, _ := strconv.Atoi(c.Param("id"))
+
+  data := map[string]interface{}{
+    "Message": "Article Show",
+    "Now":     time.Now(),
+    "ID":      id,
+  }
+
+  return render(c, "article/show.html", data)
+}
+
+func articleEdit(c echo.Context) error {
+  id, _ := strconv.Atoi(c.Param("id"))
+
+  data := map[string]interface{}{
+    "Message": "Article Edit",
+    "Now":     time.Now(),
+    "ID":      id,
+  }
+
+  return render(c, "article/edit.html", data)
 }
 
 func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
